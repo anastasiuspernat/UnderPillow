75931916de32fe2df4ae650ba0c3eb37fc038958
Anastasius <anastasiuspernat@gmail.com>
2022-11-09 08:47:20 -0800

    Improved UI

--- DiffusionInfo/ContentView.swift
+++ DiffusionInfo/ContentView.swift
@@ -12,52 +12,59 @@ struct names {
     var name = ""
 }
 
-var demoData = ["Click on the button below to select folders with images"]
+var demoData = [""]
 
 struct ContentView: View {
     @State var selectKeeper = Set<String>()
     
     var body: some View {
         HStack {
-                    List(demoData, id: \.self, selection: $selectKeeper){ name in
-                        Text(name)
-                    }.frame(maxWidth: .infinity)
-                }
-        Button(action: {
+            List(demoData, id: \.self, selection: $selectKeeper){ name in
+                Text(name)
+            }.frame(maxWidth: .infinity)
+    
+            VStack {
+                Text("Click on the button below to select folders with images").padding()
+                Button(action: {
+                                
+                    let dialog = NSOpenPanel();
                         
-            let dialog = NSOpenPanel();
-                
-                dialog.showsResizeIndicator    = true
-                dialog.showsHiddenFiles        = false
-                dialog.canChooseDirectories    = true
-                dialog.canChooseFiles = false
-                dialog.canCreateDirectories    = false
-                dialog.allowsMultipleSelection = false
+                        dialog.showsResizeIndicator    = true
+                        dialog.showsHiddenFiles        = false
+                        dialog.canChooseDirectories    = true
+                        dialog.canChooseFiles = false
+                        dialog.canCreateDirectories    = false
+                        dialog.allowsMultipleSelection = false
 
-                if (dialog.runModal() == NSApplication.ModalResponse.OK) {
-                    
-                    
-                    let connection = NSXPCConnection(serviceName: "Crispy-Driven-Pixels.DiffusionInfoXPC")
-                    connection.remoteObjectInterface = NSXPCInterface(with: DiffusionInfoXPCProtocol.self)
-                    connection.resume()
+                        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+                            
+                            
+                            let connection = NSXPCConnection(serviceName: "Crispy-Driven-Pixels.DiffusionInfoXPC")
+                            connection.remoteObjectInterface = NSXPCInterface(with: DiffusionInfoXPCProtocol.self)
+                            connection.resume()
 
-                    var service = connection.remoteObjectProxyWithErrorHandler { error in
-                        print("Received error:", error)
-                    } as? DiffusionInfoXPCProtocol
+                            var service = connection.remoteObjectProxyWithErrorHandler { error in
+                                print("Received error:", error)
+                            } as? DiffusionInfoXPCProtocol
 
-                    service?.setFolders( ["folderSettings":dialog.url?.path] ) { response in
-                        print("Response from XPC service(2):", response)
-                    }
+                            service?.setFolders( ["folderSettings":dialog.url?.path] ) { response in
+                                print("Response from XPC service(2):", response)
+                            }
 
-                    DistributedNotificationCenter.default().postNotificationName(NSNotification.Name("FoldersChanged"), object: Bundle.main.bundleIdentifier, userInfo: nil, options: [.deliverImmediately])
+                            DistributedNotificationCenter.default().postNotificationName(NSNotification.Name("FoldersChanged"), object: Bundle.main.bundleIdentifier, userInfo: nil, options: [.deliverImmediately])
 
-                } else {
-                    // User clicked on "Cancel"
-                    return
-                }
-        }, label: {
-            Text("Select folders for access")
-        }).padding()
+                        } else {
+                            // User clicked on "Cancel"
+                            return
+                        }
+                }, label: {
+                    Text("Select folders")
+                }).padding()
+                Text("Support, more information:").padding()
+                Text("Anastasiy Safari")
+                Link("https://anastasiy.com", destination: URL(string: "https://anastasiy.com")!)
+            }
+        }
     }
 }
 
